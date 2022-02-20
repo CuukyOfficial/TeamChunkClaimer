28425a9536c779a58d9103caccdaa29ead95e731
CuukyOfficial <just.cookie.jc@gmail.com>
2022-02-20 13:19:20 +0100

    Fixed chat hook stuck

--- src/de/cuuky/teamchunkclaimer/menu/team/options/GeneralOptionsMenu.java
+++ src/de/cuuky/teamchunkclaimer/menu/team/options/GeneralOptionsMenu.java
@@ -1,11 +1,11 @@
 package de.cuuky.teamchunkclaimer.menu.team.options;
 
-import de.cuuky.cfw.hooking.HookManager;
 import de.cuuky.cfw.hooking.hooks.chat.ChatHook;
 import de.cuuky.cfw.hooking.hooks.chat.ChatHookHandler;
 import de.cuuky.cfw.inventory.ItemClick;
 import de.cuuky.cfw.utils.item.BuildItem;
 import de.cuuky.cfw.version.types.Materials;
+import de.cuuky.teamchunkclaimer.ChunkClaimer;
 import de.cuuky.teamchunkclaimer.entity.player.ChunkPlayer;
 import de.cuuky.teamchunkclaimer.entity.team.ChunkTeam;
 import de.cuuky.teamchunkclaimer.menu.ChunkClaimerMenu;
@@ -23,24 +23,21 @@ public class GeneralOptionsMenu extends ChunkClaimerMenu {
 
     private ItemClick getClick(String msg, String command) {
         ChunkTeam team = player.getTeam();
-        HookManager hook = team.getHandler().getClaimer().getCuukyFrameWork().getHookManager();
+        ChunkClaimer claimer = team.getHandler().getClaimer();
 
         return (event) -> {
             this.close();
-            hook.registerHook(new ChatHook(player.getPlayer(), team.getHandler().getClaimer().getPrefix() + msg,
+            claimer.getCuukyFrameWork().getHookManager().registerHook(
+                new ChatHook(player.getPlayer(), team.getHandler().getClaimer().getPrefix() + msg,
                     new ChatHookHandler() {
-
-                @Override
-                public boolean onChat(PlayerChatEvent event) {
-                    if (!team.getHandler().getClaimer().getPlugin().getServer().dispatchCommand(player.getPlayer(),
-                            String.format(command, event.getMessage()))) {
-                        open();
-                        return true;
-                    }
-
-                    return false;
-                }
-            }));
+                        @Override
+                        public boolean onChat(PlayerChatEvent event) {
+                            team.getHandler().getClaimer().getPlugin().getServer()
+                                .dispatchCommand(player.getPlayer(), String.format(command, event.getMessage()));
+                            open();
+                            return true;
+                        }
+                    }));
         };
     }
 
@@ -59,21 +56,20 @@ public class GeneralOptionsMenu extends ChunkClaimerMenu {
         BuildItem builder = new BuildItem().material(Materials.BOOK.parseMaterial());
         ChunkTeam team = player.getTeam();
         this.addItem(11, builder.displayName("§2Name").lore("§7Wert§8: §f" + team.getName()).build(),
-                this.getClick("§7Neuen Teamnamen eingeben", "team rename %s"));
+            this.getClick("§7Neuen Teamnamen eingeben", "team rename %s"));
 
-        this.addItem(12, builder.displayName("§2Tag").lore("§7Wert§8: §f" + (team.getTag() == null ? "-" : team.getTag()))
-                        .lore("§7Wert§8: §f" + (team.getTag() == null ? "-" : team.getTag()))
-                        .build(),
-                this.getClick("§7Neuen Tag eingeben:", "team settag %s"));
+        this.addItem(12,
+            builder.displayName("§2Tag").lore("§7Wert§8: §f" + (team.getTag() == null ? "-" : team.getTag()))
+                .lore("§7Wert§8: §f" + (team.getTag() == null ? "-" : team.getTag())).build(),
+            this.getClick("§7Neuen Tag eingeben:", "team settag %s"));
 
-        this.addItem(13, builder.displayName("§2Title").lore("§7Wert§8: §f" + (team.getTitle() == null ? "-" : team.getTitle()))
-                .lore("§7Wert§8: §f" + (team.getTitle() == null ? "-" : team.getTitle()))
-                .build(),
-                this.getClick("§7Neuen Title eingeben:", "team settitle %s"));
+        this.addItem(13,
+            builder.displayName("§2Title").lore("§7Wert§8: §f" + (team.getTitle() == null ? "-" : team.getTitle()))
+                .lore("§7Wert§8: §f" + (team.getTitle() == null ? "-" : team.getTitle())).build(),
+            this.getClick("§7Neuen Title eingeben:", "team settitle %s"));
 
         this.addItem(14, builder.displayName("§2Color").lore("§7Wert§8: §f" + team.getDisplayname())
-                .lore("§7Wert§8: §f" + team.getDisplayname())
-                .build(),
-                this.getClick("§7Neue Teamfarbe eingeben:", "team setcolor %s"));
+                .lore("§7Wert§8: §f" + team.getDisplayname()).build(),
+            this.getClick("§7Neue Teamfarbe eingeben:", "team setcolor %s"));
     }
 }
